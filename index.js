732bd940efc2bf5576165d6760e99d94cc6e39fa
nit-x <132815834+nit-x@users.noreply.github.com>
2024-02-04 01:45:19 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { resolve } = require('path');
+var reload = require('reload');
+
+const app = express();
+const port = 3010;
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
